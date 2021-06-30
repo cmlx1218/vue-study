6112ad15432ed1def3e2a240d7450b0d6b15b69f
cmlx1218 <cmlx1218@163.com>
2021-06-30 20:52:00 +0800

    .feat:增加路由初始化相关

--- 04-vuerouter/src/router/index.js
+++ 04-vuerouter/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 // 配置路由相关信息
 import Router from 'vue-router'
 import Home from "../components/Home";
+import About from "../components/About";
 
 // 通过Vue.use(插件)，安装插件
 Vue.use(Router)
@@ -11,8 +12,19 @@ export default new Router({
   routes: [
     {
       path:'/',
+      redirect: '/home'
+    },
+    {
+      path:'/home',
       name: Home,
       component: () =>import('../components/Home')
+    },
+    {
+      path:'/about',
+      name: About,
+      component: () =>import('../components/About')
     }
-  ]
+  ],
+  // 使用history路由跳转就不会出现 #
+  mode: 'history'
 })
